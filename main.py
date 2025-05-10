c1d3cc685f5cf504596e69091c9a16e860137f09
bignrn <raiyo2580@gmail.com>
2025-05-10 09:14:08 +0900

    ・loggingを出力する様に実装

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import discord
 from discord.ext import commands, tasks
 import json
 from datetime import datetime, timezone, timedelta
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+# TimedRotatingFileHandlerの設定：毎月ログ切り替え
+handler = TimedRotatingFileHandler(
+    filename=f"logs/{datetime.now().strftime('%Y-%m-%d')}.log",
+    when='midnight',  # 毎日0時にチェック
+    interval=1,
+    backupCount=12,  # 最大12個まで保存（1年分）
+    encoding='utf-8',
+    utc=True
+)
+
+# ファイル名に日付を追加するフォーマット（例：bot-log.2025-05-09）
+handler.suffix = "%Y-%m-%d"
+
+# ログ設定
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[handler]
+)
+
+# 使用
+def print_out_log(msg = "ログ出力の準備が完了しています。"):
+    logging.info(msg)
+    print(msg)
 
 # トークンをjsonファイルから読み込む関数
 def load_token_from_json(path='token.json'):
@@ -25,23 +52,23 @@ TARGET_CHANNEL_NAME = "イベント通知"
 # Botが起動したときの処理
 @bot.event
 async def on_ready():
-    print(f"ログインしました: {bot.user}")
+    print_out_log(f"ログインしました: {bot.user}")
 
     for guild in bot.guilds:
-        print(f"サーバー名: {guild.name}")
+        print_out_log(f"サーバー名: {guild.name}")
 
         events = await guild.fetch_scheduled_events()
         if not events:
-            print("  現在スケジュールイベントはありません。")
+            print_out_log("  現在スケジュールイベントはありません。")
             continue
 
         for event in events:
-            print(f"  イベント名: {event.name}")
-            print(f"  開始日時: {event.start_time}")
-            print(f"  終了日時: {event.end_time}")
-            print(f"  説明: {event.description}")
-            print(f"  チャンネル: {event.channel}")
-            print(f"  種類: {event.entity_type}")
+            print_out_log(f"  イベント名: {event.name}")
+            print_out_log(f"  開始日時: {event.start_time}")
+            print_out_log(f"  終了日時: {event.end_time}")
+            print_out_log(f"  説明: {event.description}")
+            print_out_log(f"  チャンネル: {event.channel}")
+            print_out_log(f"  種類: {event.entity_type}")
 
     check_events.start()  # イベント監視ループを開始
 
@@ -66,7 +93,7 @@ async def check_events():
                     )
                     notified_event_ids.add(event.id)
                 else:
-                    print(f"⚠ チャンネル『{TARGET_CHANNEL_NAME}』が見つかりません")
+                    print_out_log(f"⚠ チャンネル『{TARGET_CHANNEL_NAME}』が見つかりません")
 
 # Bot起動
 token = load_token_from_json()
